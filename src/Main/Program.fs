9e006ac45088ec73970353b37d23ff5a1726e13b
PrsPrsBK <prsprsbk@gmail.com>
2018-04-04 21:32:07 +0900

    trial: prompt loop

--- src/Main/Program.fs
+++ src/Main/Program.fs
@@ -2,7 +2,16 @@
 
 open System
 
+let rec promptLoop () =
+    stdout.Write("prompt$ ")
+    let sth = stdin.ReadLine()
+    printfn "input is %s" sth
+    match sth with
+    | "q" -> ()
+    |_ -> promptLoop()
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    promptLoop()
     0 // return an integer exit code
