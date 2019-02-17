75233eddfd8d7aa0c1d411eabad1d2e181b9284b
PrsPrsBK <prsprsbk@gmail.com>
2019-02-18 01:48:46 +0900

    chore: separate Main from Lib

--- src/Main/Program.fs
+++ src/Main/Program.fs
@@ -1,44 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
-
-let tryParseWith f = f >> function
-    | true, v -> Some v
-    | false, _ -> None
-
-let parseAtom = tryParseWith Int32.TryParse
-
-let (|Int|_|) = parseAtom
-
-let whichAtom (x: string) =
-    match x with
-    | Int x -> "int."
-    | _ -> "symbol."
-
-let tokenize (sth: string) =
-    sth.Replace("(", " ( ").Replace(")", " ) ").Split([|" ";|], StringSplitOptions.RemoveEmptyEntries)
-
-let reader (sth: string) =
-    printfn "input is %s" sth
-    sth |> tokenize
-
-let evaler (sth: string array) =
-    for token in sth do
-        printfn "  token %s is %s" token (whichAtom token)
-    sprintf "evaled: %A" sth
-
-let printer (sth: string) =
-    printfn "%s" sth
-
-let rec looper () =
-    stdout.Write("prompt$ ")
-    let sth = stdin.ReadLine()
-    match sth with
-    | "q" -> ()
-    |_ -> sth |> reader |> evaler |> printer |> looper
+open Randori
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    looper()
+    printfn "Hello World from F#! %s" (Randori.Say.hello("from Lib"))
+    
+    if argv.Length = 0
+    then
+        Randori.LispRepl.looper()
+    else
+        match argv.[0] with
+        | "repl" -> LispRepl.looper()
+        | _ -> ()
     0 // return an integer exit code
