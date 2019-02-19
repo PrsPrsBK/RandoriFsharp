444a02c445321735947e171283bc5f4015a11046
PrsPrsBK <prsprsbk@gmail.com>
2019-02-20 00:34:36 +0900

    test: Add more Rot13

--- test/Randori.Tests/Rot13.fs
+++ test/Randori.Tests/Rot13.fs
@@ -6,10 +6,25 @@ open System
 [<Tests>]
 let tests =
   testList "Rot13" [
-    testCase "Test Case 1" <| fun _ ->
+    testCase "Oneway-Convert" <| fun _ ->
       Expect.equal
         ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         |> Randori.Rot13.convertSeq |> String.Concat)
         "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
         "Oneway Done."
+
+    testCase "Round trip" <| fun _ ->
+      Expect.equal
+        ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        |> Randori.Rot13.convertSeq |> String.Concat
+        |> Randori.Rot13.convertSeq |> String.Concat)
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "Round trip Done."
+
+    testCase "Non-Alphabet" <| fun _ ->
+      Expect.equal
+        ("テスト表abcdefgÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
+        |> Randori.Rot13.convertSeq |> String.Concat)
+        "テスト表nopqrstÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
+        "Non-Alphabet Done."
   ]
