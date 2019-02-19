17e78aca6a0afc12af40aec34c771a1bc84597c9
PrsPrsBK <prsprsbk@gmail.com>
2019-02-20 01:01:02 +0900

    test: fix little

--- test/Randori.Tests/Rot13.fs
+++ test/Randori.Tests/Rot13.fs
@@ -7,6 +7,7 @@ open System
 let tests =
   testList "Rot13" [
     testCase "Oneway-Convert" <| fun _ ->
+      // need Fully Qualified Name like a `Randori.Rot13.foo`
       Expect.equal
         ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         |> Randori.Rot13.convertSeq |> String.Concat)
@@ -23,8 +24,8 @@ let tests =
 
     testCase "Non-Alphabet" <| fun _ ->
       Expect.equal
-        ("テスト表abcdefgÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
+        ("テスト表abcdefg0123456789ÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
         |> Randori.Rot13.convertSeq |> String.Concat)
-        "テスト表nopqrstÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
+        "テスト表nopqrst0123456789ÁÉÍÓÚÝÀÈÌÒÙÂÊÎÔÛÄËÏÖÜŸáéíóúýàèìòùâêîôûäëïöüÿÇŞÃÕÑĄĘĮŲÆŒØĲẞÞǷçşãõñąęįųæœøĳßþƿ"
         "Non-Alphabet Done."
   ]
